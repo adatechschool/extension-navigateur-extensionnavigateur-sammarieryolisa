c4a0e0367fd75b885b1efaff864a28f33d821859
Ryo Rafael <ryorafael18@gmail.com>
2024-01-18 15:56:36 +0100

    prettier content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,33 @@
 // FONCTION PRINCIPALE : récupérer le mot sélectionné
 var selection = window.getSelection().toString().toLowerCase();
 
-if (selection){
-    (async () => {
+if (selection) {
+  (async () => {
+    // sauvegarder le mot
+    await chrome.storage.local.get(
+      { pastResearches: [] },
+      function (storageData) {
+        storageData.pastResearches.push({ word: selection, translation: "" });
+        console.log(storageData.pastResearches[0]);
+        chrome.storage.local.set(
+          { pastResearches: storageData.pastResearches },
+          function () {
+            console.log("Word saved:", selection);
+          }
+        );
+      }
+    );
 
-        // sauvegarder le mot
-        await chrome.storage.local.get({ pastResearches: [] }, function (storageData) {
-            storageData.pastResearches.push({word:selection, translation:""});
-            console.log(storageData.pastResearches[0]);
-            chrome.storage.local.set(
-                { pastResearches: storageData.pastResearches },
-                function () {
-                    console.log("Word saved:", selection);
-                }
-            );
-        });
-        
-        // envoyer la sélection vers le background script pour ouvrir un onglet
-        const responsea = await chrome.runtime.sendMessage({type: "toBeSearched", searchWord: selection});        
+    // envoyer la sélection vers le background script pour ouvrir un onglet
+    const responsea = await chrome.runtime.sendMessage({
+      type: "toBeSearched",
+      searchWord: selection,
+    });
 
-        // transmettre le mot à la sidebar pour l'lafficher
-        const responseb = await chrome.runtime.sendMessage({type: "updateList", wordToBeSaved: selection});
-
-        })();
+    // transmettre le mot à la sidebar pour l'lafficher
+    const responseb = await chrome.runtime.sendMessage({
+      type: "updateList",
+      wordToBeSaved: selection,
+    });
+  })();
 }
-
